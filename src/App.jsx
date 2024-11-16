f63c4f73c69cb90c6cea50bddef5030d9a6d4ab2
Jhay Savi <jansavinon@outlook.com>
2024-11-16 14:18:59 -0300

    add app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Chat from "./Components/Chat";
+
+function App() {
+  return (
+    <div>
+      <h1>Periódicos Talk</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
