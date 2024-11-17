713b5b8ec8c2e31f385359cfcdbf1ec5992e3e40
unknown <fjcorreia@outlook.com>
2024-11-17 15:28:00 -0300

    integração link

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // URL do Flask
+  baseURL: 'http://127.0.0.1:5000', // URL do Flask
 });
 
 export default api;
