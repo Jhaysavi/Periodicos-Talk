d5d80fb6babec63b2679ab22edebfe1c20f39fde
Jhay Savi <jansavinon@outlook.com>
2024-11-17 15:48:24 -0300

    api add

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: 'http://127.0.0.1:5000', // URL do Flask
 });
 
+export const searchArticles = async (query) => {
+  try {
+    const response = await api.post("/search", { query });
+    return response.data; // Retorna os resultados formatados
+  } catch (error) {
+    console.error("Error fetching articles:", error.response || error.message);
+    return null; 
+  }
+};
+
 export default api;
